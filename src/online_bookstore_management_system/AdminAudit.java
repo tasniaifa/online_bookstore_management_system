ec8d8bbe0e39988003dd3890365756aa089131c3
Kazi Tasnia Farin <tasniaifa961@gmail.com>
2025-11-02 13:39:32 +0600

    Update AdminAudit.java

--- src/online_bookstore_management_system/AdminAudit.java
+++ src/online_bookstore_management_system/AdminAudit.java
@@ -1,4 +1,3 @@
-
 package online_bookstore_management_system;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,9 +8,8 @@ import java.util.Date;
  */
 public class AdminAudit {
     public static void log(String message) {
-        try (FileWriter fw = new FileWriter("admin_audit.log", true)) {
-            fw.write(new Date() + " - " + message + "");
-        } catch (IOException e) { /* ignore */ }
+        try (FileWriter fw = new FileWriter("admin_audit.log", true)) { //record/log a message
+            fw.write(new Date() + " - " + message + ""); // handle file writing
+        } catch (IOException e) { /* ignore */ }      
     }
-    
 }
